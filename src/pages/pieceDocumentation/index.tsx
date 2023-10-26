c7d67cef92b0015512a73b3c14dfe17da22c3334
Vinicius Vaz <vvaz@beta.team>
2023-10-26 08:29:00 -0300

    docs tables

--- src/pages/pieceDocumentation/index.tsx
+++ src/pages/pieceDocumentation/index.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useCallback } from 'react';
 import Layout from '@theme/Layout';
 import { useLocation } from 'react-router-dom';
 import { useGallery } from '@site/src/utils/useGallery';
+import { Link } from 'react-router-dom';
+import './index.css'
+
 
 const PieceDocumentationPage = () => {
     const location = useLocation();
@@ -10,62 +13,143 @@ const PieceDocumentationPage = () => {
     let params = new URLSearchParams(location.search);
     const repository = params.get('repository');
     const pieceName = params.get('piece');
+    
     const pieceData = galleryPieces[repository]['pieces'][pieceName];
+    var inputProperties = pieceData?.input_schema?.properties || {};
+    inputProperties = Object.keys(inputProperties).length === 0 ? null : inputProperties;
+    const inputDefinitions = pieceData?.input_schema?.definitions || null;
 
-    // Extracting input properties from JSON
+    var outputProperties = pieceData?.output_schema?.properties || {};
+    outputProperties = Object.keys(outputProperties).length === 0 ? null : outputProperties; 
+    const outputDefinitions = pieceData?.output_schema?.definitions || null;
 
-    // Define a CSS class for styling the table
-    const tableStyle: React.CSSProperties = {
-        margin: 'auto', 
-        borderCollapse: 'collapse',
-    };
+    var secretsProperties = pieceData?.secrets_schema?.properties || {};
+    secretsProperties = Object.keys(secretsProperties).length === 0 ? null : secretsProperties; 
+    const secretsDefinitions = pieceData?.secrets_schema?.definitions || null;
+    
+    const getTableBodyRow = useCallback((inputArg, properties, definitions) => {
+        const itemSchema = properties[inputArg]
+        var type = itemSchema.format ? itemSchema.format : itemSchema.type;
+        var name = itemSchema.title
+        if ("allOf" in itemSchema || "oneOf" in itemSchema || "anyOf" in itemSchema) {
+            const ofKey = "allOf" in itemSchema ? "allOf" : "oneOf" in itemSchema ? "oneOf" : "anyOf";
+            const definitionName = itemSchema[ofKey][0].$ref.split("/").pop() as string;
+            const definition = definitions[definitionName];
+            type = `enum[${definition.type}]`;
+            name = definition.title;
+        }
+        type = type === "number" ? "float" : (type as string);
 
-    const thStyle: React.CSSProperties = {
-        backgroundColor: '#f2f2f2',
-        padding: '8px',
-        textAlign: 'left',
-    };
+        if (type === "array" && itemSchema?.items?.type) {
+            const itemsType = itemSchema.items.type;
+            type = `array[${itemsType}]`;
+        }else if (type === 'array' && itemSchema?.items?.$ref) {
+            type = `array[object]`
+        }
 
-    const tdStyle: React.CSSProperties = {
-        border: '1px solid #dddddd',
-        padding: '8px',
-        textAlign: 'left',
-    };
-
-    const inputProperties = pieceData.input_schema.properties;
+        return (
+            <tr key={inputArg}>
+                <td>{name}</td>
+                <td>{inputArg}</td>
+                <td>{
+                    type
+                }</td>
+                <td>{properties[inputArg].description}</td>
+            </tr>
+        )
+    }, [])
 
     return (
         <Layout title="Piece Documentation" description="">
-            <div >
-                <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-                    <h1>{pieceData.style?.node_label ? pieceData.style?.node_label : pieceData.name}</h1>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-                    <h4>Input Arguments</h4>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
-                    <table style={tableStyle}>
-                        <thead>
-                            <tr>
-                                <th style={thStyle}>Input Name</th>
-                                <th style={thStyle}>Input Arg</th>
-                                <th style={thStyle}>Data Type</th>
-                                <th style={thStyle}>Description</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.keys(inputProperties).map((inputArg) => (
-                                <tr key={inputArg}>
-                                    <td style={tdStyle}>{inputProperties[inputArg].title}</td>
-                                    <td style={tdStyle}>{inputArg}</td>
-                                    <td style={tdStyle}>{
-                                        inputProperties[inputArg].type
-                                    }</td>
-                                    <td style={tdStyle}>{inputProperties[inputArg].description}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+            <div className='container'>
+                <div className='piece-documentation-container'>
+                    <div style={{ display: 'flex', textAlign: 'center', justifyContent: 'space-between'}}>
+                        <h1>{pieceData.style?.node_label ? pieceData.style?.node_label : pieceData.name}</h1>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'left', textAlign: 'center', marginTop: '20px' }}>
+                        <h3>Input Arguments</h3>
+                    </div>
+                    {
+                        inputProperties ? (
+                            <>
+                                <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+                                    <table className='args-table'>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Argument</th>
+                                                <th>Data Type</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {Object.keys(inputProperties).map((inputArg) => (
+                                                getTableBodyRow(inputArg, inputProperties, inputDefinitions)
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </>
+                        ) : (
+                            <p>This piece has no inputs.</p>
+                        )
+                    }
+                    <div style={{ display: 'flex', justifyContent: 'left', textAlign: 'center', marginTop: '20px' }}>
+                        <h3>Output Arguments</h3>
+                    </div>
+                    {
+                        outputProperties ? (
+                            <>
+                            <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+                                <table className='args-table'>
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Argument</th>
+                                            <th>Data Type</th>
+                                            <th>Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {Object.keys(outputProperties).map((outputArg) => (
+                                            getTableBodyRow(outputArg, outputProperties, outputDefinitions)
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                            </>
+                        ) : (
+                            <p>This piece has no outputs.</p>
+                        )
+                    }
+                    <div style={{ display: 'flex', justifyContent: 'left', textAlign: 'center', marginTop: '20px' }}>
+                        <h3>Secrets Arguments</h3>
+                    </div>
+                    {
+                        secretsProperties ? (
+                            <>
+                                <div style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }}>
+                                    <table className='args-table'>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Argument</th>
+                                                <th>Data Type</th>
+                                                <th>Description</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {Object.keys(secretsProperties).map((secretsArg) => (
+                                                getTableBodyRow(secretsArg, secretsProperties, secretsDefinitions)
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </>
+                        ) : (
+                            <p>This piece has no secrets.</p>
+                        )
+                    }
                 </div>
             </div>
         </Layout>
