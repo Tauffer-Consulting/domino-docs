2f088eac8561500c44ae912535cf433265d2febd
Vinicius Vaz <vvaz@beta.team>
2023-10-27 07:18:29 -0300

    search bar

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Layout from '@theme/Layout';
 import PieceCard from '@site/src/components/Gallery/PieceCard';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -8,13 +8,55 @@ import { useGallery } from '@site/src/utils/useGallery';
 
 function Gallery() {
 
-    const galleryPieces = useGallery();
+    const allGalleryPieces = useGallery();
+
+    const [galleryPieces, setGalleryPieces] = useState(allGalleryPieces);
+
+    useEffect(() => {
+        setGalleryPieces(allGalleryPieces)
+    }, [allGalleryPieces])
+
+    const handleSearch = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        // Search over galleryPieces and return exactly the same structure but with the filtered pieces
+        const searchValue = event.target.value.trim().toLowerCase();
+
+        if (searchValue.length === 0) {
+            setGalleryPieces(allGalleryPieces)
+        }
+        const filteredPieces = Object.keys(allGalleryPieces).reduce((acc, repositoryKey) => {
+            const repository = allGalleryPieces[repositoryKey];
+            const pieces = repository.pieces;
+            const filteredPieces = Object.keys(pieces).reduce((acc, pieceKey) => {
+                const piece = pieces[pieceKey];
+                if (piece.name.toLowerCase().includes(searchValue) || piece?.style?.node_label?.toLowerCase().includes(searchValue)) {
+                    acc[pieceKey] = piece;
+                }
+                return acc;
+            }, {});
+            if (Object.keys(filteredPieces).length > 0) {
+                acc[repositoryKey] = {
+                    ...repository,
+                    pieces: filteredPieces,
+                };
+            }
+            return acc;
+        }, {});
+        setGalleryPieces(filteredPieces)
+
+    }, [allGalleryPieces])
 
     return (
         <Layout title="Gallery" description="">
             <div className='container'>
                 <h1 style={{ marginTop: '20px' }}>Gallery</h1>
                 <p>This section lists all the open source pieces repositories audited by Domino.</p>
+                <div className='search-bar-div'>
+                    <input 
+                        placeholder='Search for pieces' 
+                        className='search-bar-input'
+                        onChange={handleSearch}
+                    />
+                </div>
                 <div className="card-container">
                     {Object.keys(galleryPieces).map((repositoryKey) => {
                         const repository = galleryPieces[repositoryKey];
