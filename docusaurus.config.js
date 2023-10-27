72ee6ddde6c8c63f4adcac7fed12cb9fc270e6f2
luiz <luiz_tauffer@hotmail.com>
2023-10-27 18:24:25 +0200

    fix logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
         title: '',
         logo: {
           alt: 'Domino Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
