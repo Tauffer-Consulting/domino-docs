855d75e74384d665083366a1b9677dc7b041bb54
luiz <luiz_tauffer@hotmail.com>
2023-12-01 18:43:47 +0100

    features

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -14,8 +14,16 @@ const FeatureList: FeatureItem[] = [
     imageUrl: require('@site/static/img/landing/visual-workflows-transparent.png').default,
     description: (
       <>
-        Domino brings an intuitive Graphical User Interface that facilitates creating,
-        editing and monitoring any type of Workflow, from data processing to machine learning.
+        <p>
+          Domino brings an intuitive Graphical User Interface that facilitates creating,
+          editing and monitoring any type of workflow, including integrations with:
+        </p>
+        <ul className={styles.ulFeatureList}>
+          <li><span className={styles.checkmark}>✔</span>Advanced data processing tools</li>
+          <li><span className={styles.checkmark}>✔</span>Machine learning and generative AI algorithms</li>
+          <li><span className={styles.checkmark}>✔</span>External APIs and social media</li>
+          <li><span className={styles.checkmark}>✔</span>and more!</li>
+        </ul>
       </>
     ),
     url: '/docs/domino_components/domino_components_gui',
@@ -25,8 +33,16 @@ const FeatureList: FeatureItem[] = [
     imageUrl: require('@site/static/img/landing/pieces-sharing-transparent.png').default,
     description: (
       <>
-        Domino brings a standard for writing and publishing functional Pieces
-        which makes them reusable and reproducible, and can be easily shared with the community.
+        <p>
+          Domino proposes a standard for writing and publishing functional Pieces
+          which makes them reusable and reproducible, and can be easily shared with the community.
+        </p>
+        <ul className={styles.ulFeatureList}>
+          <li><span className={styles.checkmark}>✔</span>Standardized approach to Pieces creation</li>
+          <li><span className={styles.checkmark}>✔</span>Bring any functional code to the graphical interface</li>
+          <li><span className={styles.checkmark}>✔</span>Easily share your PIeces with the community</li>
+          <li><span className={styles.checkmark}>✔</span>Easily install and use the community Pieces</li>
+        </ul>
       </>
     ),
     url: '/docs/pieces',
@@ -36,8 +52,15 @@ const FeatureList: FeatureItem[] = [
     imageUrl: require('@site/static/img/landing/workflow-monitoring-transparent.png').default,
     description: (
       <>
-        Experience a clean and user-friendly interface for monitoring workflows results,
-        including logs and rich reports with images, interactive graphics, and tables.
+        <p>
+          Experience a clean and user-friendly interface for monitoring workflows results,
+          including logs and rich reports with images, interactive graphics, and tables.
+        </p>
+        <ul className={styles.ulFeatureList}>
+          <li><span className={styles.checkmark}>✔</span>Intuitive and easy to use interface for workflows monitoring</li>
+          <li><span className={styles.checkmark}>✔</span>Real-time monitoring of status and logs</li>
+          <li><span className={styles.checkmark}>✔</span>Detailed reporting of results with text, tables, images and interactive graphics</li>
+        </ul>
       </>
     ),
     url: '/docs/domino_components/domino_components_gui',
@@ -49,16 +72,16 @@ function Feature({ title, imageUrl, description, isImageRight, url }: { isImageR
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: '3rem',
+    marginBottom: '2.5rem',
   };
 
   return (
     <div className="row" style={rowStyle}>
       {isImageRight ? (
         <div className="col col--6" style={{ paddingRight: '1rem' }}>
-          <div className="text--center padding-horiz--md">
+          <div className="text--left padding-horiz--sm">
             <h3><a href={url} className={styles.featureTitle}>{title}</a></h3>
-            <p>{description}</p>
+            <div>{description}</div>
           </div>
         </div>
       ) : null}
@@ -71,9 +94,9 @@ function Feature({ title, imageUrl, description, isImageRight, url }: { isImageR
 
       {!isImageRight ? (
         <div className="col col--6" style={{ paddingLeft: '1rem' }}>
-          <div className="text--center padding-horiz--md">
+          <div className="text--left padding-horiz--sm">
             <h3><a href={url} className={styles.featureTitle}>{title}</a></h3>
-            <p>{description}</p>
+            <div>{description}</div>
           </div>
         </div>
       ) : null}
