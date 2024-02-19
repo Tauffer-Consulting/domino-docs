8cd8cf76619a0c68dd149e5d8d4b6ff9bc386745
vinicvaz <vinicvaz95@gmail.com>
2024-02-19 14:22:17 -0300

    favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Domino',
   tagline: 'Build amazing ideas, piece by piece.',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon/favicon.ico',
 
   // Set the production url of your site here
   url: 'https://tauffer-consulting.github.io',
