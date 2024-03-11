a0793b2d5fec70a12c1669e5a8e1cee34bc7c1c3
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2024-03-11 08:54:58 -0300

    chore: improve responsive styles

--- src/components/Home/Navbar/Hanburger/index.tsx
+++ src/components/Home/Navbar/Hanburger/index.tsx
@@ -1,33 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Icon } from '@iconify/react'
 import "./index.css"
 
 interface Props {
-  isOpen: boolean
-  size: number
-  toggle: React.Dispatch<React.SetStateAction<boolean>>
   menuItems: {
     label: string;
     link: string;
   }[]
 }
 
-export const Hamburger: React.FC<Props> = ({ isOpen, size, toggle, menuItems }) => {
+export const Hamburger: React.FC<Props> = ({ menuItems }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
-  const handleIconClick = () => {
-    toggle(!isOpen);
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
-    <div id="hamburger" onClick={handleIconClick}>
-      <Icon icon="ic:baseline-menu" fontSize={size} />
-      <div className={isOpen ? 'menu' : 'menu close'}>
-        {menuItems.map(({ label, link }) => (
-          <div className="menuitem">
-            <a href={link}>{label}</a>
-          </div>
-        ))}
-      </div>
+    <div className="hamburger-menu">
+      <button className="menu-button" onClick={toggleMenu}>
+        <Icon icon="ic:baseline-menu" fontSize={36} />
+      </button>
+      {isOpen && (
+        <div className="menu-list" ref={menuRef}>
+          <ul>
+            {menuItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.link}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Home/Navbar/index.tsx
+++ src/components/Home/Navbar/index.tsx
@@ -2,58 +2,60 @@ import React, { useMemo, useState } from 'react'
 import "./index.css"
 import { Hamburger } from './Hanburger';
 import { Icon } from '@iconify/react'
+import { useWindowDimensions } from '@site/src/utils/useWindowSize';
 
 export const Navbar = () => {
-
-  const [isOpen,setIsOpen] = useState(false);
-
-  const menuItems = useMemo(()=>{
+  const { width } = useWindowDimensions();
+  const menuItems = useMemo(() => {
     return [
-      {label:"Documentation", link:"#session-1"},
-      {label:"Blog", link:"#session-2"},
-      {label:"Gallery", link:"#session-3"},
-      {label:"About us", link:"#"},
+      { label: "Documentation", link: "#session-1" },
+      { label: "Blog", link: "#session-2" },
+      { label: "Gallery", link: "#session-3" },
+      { label: "About us", link: "#" },
     ]
-  },[])
-
+  }, [])
 
   return (
-    <nav id="navbar">
-      <img id="logo" src="brand/png/main_logo.png" alt="Logo" />
+    <>
+      {width <= 1144 ? (
+        <nav id="navbar-small">
+          <img id="logo" src="brand/png/main_logo.png" alt="Logo" />
+          <Hamburger
+            menuItems={menuItems}
+          />
+        </nav>
+      ) : (
+        <nav id="navbar">
+          <img id="logo" src="brand/png/main_logo.png" alt="Logo" />
 
-      <Hamburger 
-        isOpen={isOpen} 
-        size={36} 
-        toggle={setIsOpen} 
-        menuItems={menuItems}
-      />
-
-      <ul id="central-links">
-        {menuItems.map(({label,link})=>
-          <li>
-            <a href={link}>
-              {label}
-            </a>
-          </li>
-        )}
-      </ul>
-      <ul id="right-buttons">
-        <li>
-          <button id="primary">
-            Try cloud
-          </button>
-        </li>
-        <li>
-          <button id="secondary">
-            Contact
-          </button>
-        </li>
-        <li>
-          <a id="github">
-            <Icon icon="fa-brands:github" fontSize={36} />
-          </a>
-        </li>
-      </ul>
-    </nav>
+          <ul id="central-links">
+            {menuItems.map(({ label, link }) =>
+              <li>
+                <a href={link}>
+                  {label}
+                </a>
+              </li>
+            )}
+          </ul>
+          <ul id="right-buttons">
+            <li>
+              <button id="primary">
+                Try cloud
+              </button>
+            </li>
+            <li>
+              <button id="secondary">
+                Contact
+              </button>
+            </li>
+            <li>
+              <a id="github">
+                <Icon icon="fa-brands:github" fontSize={36} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   )
 }
